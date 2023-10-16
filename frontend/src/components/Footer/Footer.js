330c6d41ee4047adcf99c64143e825ce4b25ec5c
Taylor Musolf <tcmusolf@gmail.com>
2023-10-16 13:44:16 -0700

    add links to footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import {BsGithub, BsLinkedin} from 'react-icons/bs'
 import {AiOutlineMail, AiFillBug} from 'react-icons/ai'
+import {SiWellfound} from 'react-icons/si';
 
 function Footer(){
 
@@ -10,8 +11,8 @@ function Footer(){
                 <div className="left-footer">
                     <h1>Suggestions for improvement?</h1>
                     <div className="footer-links">
-                        <a href = "mailto: tcmusolf@gmail.com"><AiOutlineMail /></a>
-                        <a href = "mailto: tcmusolf@gmail.com"><AiFillBug /></a>
+                        <Link to={{pathname:'mailto: tcmusolf@gmail.com'}} target='_blank'><AiOutlineMail /></Link>
+                        <Link to={{pathname:'https://github.com/taylormusolf/ChatAi/issues'}} target='_blank'><AiFillBug /></Link>
                     </div>
 
                 </div>
@@ -25,6 +26,7 @@ function Footer(){
                     <div className="footer-links">
                         <Link to={{pathname:'https://github.com/taylormusolf/aibot_creator'}} target='_blank'>< BsGithub/></Link>
                         <Link to={{pathname:'https://www.linkedin.com/in/taylor-musolf'}} target='_blank'>< BsLinkedin/></Link>
+                        <Link to={{pathname: 'https://wellfound.com/u/taylor-musolf'}} target='_blank'><SiWellfound /></Link>
                     </div>
                 </div>
             </div>
